db0ba8f573fdedce57991523805fe3eec4a8b3f1
余声里 <64511565+yuusheng@users.noreply.github.com>
2022-09-15 17:05:34 +0800

    refactor: line breaker reg

--- packages/md-parser/src/headerParser.ts
+++ packages/md-parser/src/headerParser.ts
@@ -1,8 +1,16 @@
 import { headerContent } from './types'
 
 export const headerReg = /---\r?\n(.*?)\r?\n---/is
+
+export function getLineBreakerReg(option?: string) {
+  return new RegExp('\r?\n', option)
+}
+
 export function headerInterpreter(content: string) {
-  const matchArr = content.match(headerReg).splice(1)[0].split(/\r?\n/)
+  const matchArr = content
+    .match(headerReg)
+    .splice(1)[0]
+    .split(getLineBreakerReg())
 
   return matchArr.reduce((pre, cur) => {
     const [key, value] = cur.split(':').map((v) => v.replace(/\s/g, ''))
