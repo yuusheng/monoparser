005b64603e90927c289bcdf3431aa59efd1e59f5
余声里 <64511565+yuusheng@users.noreply.github.com>
2022-09-15 16:59:31 +0800

    fix: windows line break err

--- packages/md-parser/src/headerParser.ts
+++ packages/md-parser/src/headerParser.ts
@@ -1,6 +1,6 @@
 import { headerContent } from './types'
 
-export const headerReg = /---\n(.*?)\n---/is
+export const headerReg = /---\r?\n(.*?)\r?\n---/is
 export function headerInterpreter(content: string) {
   const matchArr = content.match(headerReg).splice(1)[0].split('\n')
 
